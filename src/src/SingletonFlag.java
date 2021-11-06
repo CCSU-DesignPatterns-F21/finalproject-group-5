5f2870b11c9aea0997f66d00e57b3d3d2d2bf8bb
mbgccsu <michaelgieras@my.ccsu.edu>
2021-11-05 21:34:06 -0400

    Instantiated singleton and added drawFlag() method.
    Updated code with instantiation of singleton. Added drawFlag method.

---
+++ src/src/SingletonFlag.java
@@ -0,0 +1,25 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.event.*;
+
+
+
+public class SingletonFlag{
+	
+	public static SingletonFlag getInstance(){
+		if (theInstance == null) {
+			theInstance = new SingletonFlag();
+		}
+		return theInstance;
+	}
+	private SingletonFlag() {
+	}
+	private static SingletonFlag theInstance = null;
+	private Graphics2D g2d;
+	public void drawFlag() {
+		g2d.drawRect(evilflagx, evilflagy, 30, 30);
+	}
+}
