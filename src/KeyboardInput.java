af4bbc30e6c6cc95a744525eb83a67a9a4498ca5
sledzz <sledzryan@gmail.com>
2021-11-28 19:06:25 -0500

    Attempt to make character cycling

--- src/KeyboardInput.java
+++ src/KeyboardInput.java
@@ -1,12 +1,19 @@
 package designProject;
 
 import java.awt.Graphics;
-
+import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
-class KeyboardInput extends KeyAdapter  {
+class KeyboardInput extends KeyAdapter implements KeyListener {
 	
 	Speed fastKnight = new FastSpeedDecorator((Speed) new Knight()); 
 	int fastSpeed = fastKnight.get_velocity();
@@ -18,52 +25,80 @@ class KeyboardInput extends KeyAdapter  {
 	int slowSpeed = slowSpeedDefender.get_velocity();
 	
 	
+	//Object decorator_arr[][] = {{fastKnight,  fastSpeed},{extraFastRunner, extraFastSpeed}, {slowSpeedDefender, slowSpeed}};
+	
+	Map<Speed, Integer> character_map = Map.of(fastKnight, fastSpeed, extraFastRunner, extraFastSpeed, slowSpeedDefender, slowSpeed);
+		
+	public boolean toggled(KeyEvent e) {
+		boolean pressed = false;
+		if(e.getKeyCode() == KeyEvent.VK_Q) {
+			pressed = true;
+		}
+		return pressed;
+	}
+
 	
 	public void keyPressed( KeyEvent e) {
 		
+		//Knight is default
+		Iterator<Entry<Speed, Integer>> map_iter = character_map.entrySet().iterator();
+
+		Map.Entry<Speed,Integer> kv = map_iter.next();	
 		
-		if (e.getKeyCode()== KeyEvent.VK_W) {
+		Speed key = kv.getKey();
+		Integer value = kv.getValue();
+		System.out.println(key);
+		System.out.println(value);
+		
+		
+		if (toggled(e) == true) {
+			key = map_iter.next().getKey();
+			value = map_iter.next().getValue();
+			System.out.println("entered");
+			System.out.println(key);
+			System.out.println(value);
+		}
+	
+		
+		//Speed key3 = map_iter.next().getKey();
+		//Integer value3 = map_iter.next().getValue();
+		//System.out.println(key3);
+		//System.out.println(value3);
+		
+		
+		else if (e.getKeyCode()== KeyEvent.VK_W) {
 			Game.changeY(-1);
-			fastKnight.changeY(-1,fastSpeed);
-			extraFastRunner.changeY(-1,extraFastSpeed);
-			slowSpeedDefender.changeY(-1, slowSpeed);
+			key.changeY(-1, value);
 			}
-		
+			
 		else if (e.getKeyCode()== KeyEvent.VK_A) {
 			Game.changeX(-1);
-			fastKnight.changeX(-1,fastSpeed);
-            extraFastRunner.changeX(-1,extraFastSpeed);
-            slowSpeedDefender.changeX(-1, slowSpeed);
+			key.changeX(-1,value);						
 			}
-		
+				
 		else if (e.getKeyCode()== KeyEvent.VK_S) {
 			Game.changeY(1);
-			fastKnight.changeY(1,fastSpeed);
-			extraFastRunner.changeY(1,extraFastSpeed);
-			slowSpeedDefender.changeY(1,slowSpeed);
+			key.changeY(1,value);
 			}
-		
+				
 		else if (e.getKeyCode()== KeyEvent.VK_D) {
 			Game.changeX(1);
-			fastKnight.changeX(1,fastSpeed);
-            extraFastRunner.changeX(1, extraFastSpeed);
-            slowSpeedDefender.changeX(1,slowSpeed);
+			key.changeX(1,value);
 			}
 		
-		 else if (e.getKeyCode()== KeyEvent.VK_CONTROL){
+		else if (e.getKeyCode()== KeyEvent.VK_CONTROL){
              Originator.saveState();
              
 		 	}
 		
-		 else if (e.getKeyCode()== KeyEvent.VK_DELETE) {
+		else if (e.getKeyCode()== KeyEvent.VK_DELETE) {
              Originator.loadState();
-		 }
-	}
-	
+			}
+		
+		}
+
 	public void keyTyped(KeyEvent e) {}
 	public void keyReleased(KeyEvent e) {}
-
-	
 
 	
    
