3ebf410f7cf5081c6888f60394c7c7148bf70910
silcreteantelope <ballmax23@gmail.com>
2021-11-30 20:18:42 -0500

    Revert "Ryan"

--- src/Defender.java
+++ src/Defender.java
@@ -1,27 +1,19 @@
-package designProject;
-
-import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.event.KeyEvent;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
-import java.awt.event.KeyEvent;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import java.awt.event.*;
 
-public class Defender implements Speed, Character{
+/**
+ * Creates defender using interface character.
+ */
+public class Defender implements Character{
 	
-	private static int tri_X_coordinates1 [] = {175,180,185};
-	private static int tri_X_coordinates2 [] = {185,190,195};
-	private static int tri_X_coordinates3 [] = {195,200,205};
-	private static int tri_y_coordinates_enemy [] = {60,70, 60};
-	private static int tri_y_coordinates_player [] = {300,290, 300};
-	private static int rect_X_coordinate_enemy = 175;
-	private static int rect_y_coordinate_enemy= 30;
-	private static int rect_X_coordinate_player = 175;
-	private static int rect_y_coordinate_player= 300;
+	private int tri_X_coordinates1 [] = {175,180,185};
+	private int tri_X_coordinates2 [] = {185,190,195};
+	private int tri_X_coordinates3 [] = {195,200,205};
+	private int tri_y_coordinates_enemy [] = {60,70, 60};
+	private int tri_y_coordinates_player [] = {300,290, 300};
+	private int rect_X_coordinate_enemy = 175;
+	private int rect_y_coordinate_enemy= 30;
+	private int rect_X_coordinate_player = 175;
+	private int rect_y_coordinate_player= 300;
 	private int character_width =30;
 	private int character_length= 30;
 	private int crown_points =3;
@@ -29,96 +21,35 @@ public class Defender implements Speed, Character{
 
 	@Override
 	public void draw_character(Graphics g) {
-		/*Graphics enemy_defender = (Graphics) g;
+		Graphics enemy_defender = (Graphics) g;
 		
 		enemy_defender.fillRect(rect_X_coordinate_enemy, rect_y_coordinate_enemy, character_width, character_length);
 		enemy_defender.fillPolygon(tri_X_coordinates1, tri_y_coordinates_enemy,crown_points);
 		enemy_defender.fillPolygon(tri_X_coordinates2, tri_y_coordinates_enemy,crown_points);
 		enemy_defender.fillPolygon(tri_X_coordinates3, tri_y_coordinates_enemy,crown_points);
-		*/
 		
 		Graphics player_defender = (Graphics) g;
 		
-	
-		//crown base
-		g.setColor(Color.ORANGE);
-		
-		//crown point
 		player_defender.fillRect(rect_X_coordinate_player, rect_y_coordinate_player, character_width, character_length);
-		g.setColor(Color.ORANGE);
 		player_defender.fillPolygon(tri_X_coordinates1, tri_y_coordinates_player,crown_points);
-		g.setColor(Color.ORANGE);
 		player_defender.fillPolygon(tri_X_coordinates2, tri_y_coordinates_player,crown_points);
-		g.setColor(Color.ORANGE);
 		player_defender.fillPolygon(tri_X_coordinates3, tri_y_coordinates_player,crown_points);
 		
-		//crown jewels
-		g.setColor(Color.red);
-		player_defender.fillOval(rect_X_coordinate_player+3, rect_y_coordinate_player-11, 4, 5);
-		
-		g.setColor(Color.black);
-		player_defender.fillOval(rect_X_coordinate_player+13, rect_y_coordinate_player-11, 4, 5);
-		
-		g.setColor(Color.blue);
-		player_defender.fillOval(rect_X_coordinate_player+23, rect_y_coordinate_player-11, 4, 5);
 		
 	}
 	
-	@Override
-	public int get_velocity() {
-		return 1;
-	}
 	
-	int velocity= get_velocity();
 	
-	public void changeX(int offset, int velocity) {
-		
-		
-		rect_X_coordinate_player +=(offset*velocity);
-		rect_X_coordinate_enemy +=(offset*velocity);
-		
-		
-		for(int i=0; i<tri_X_coordinates1.length; i++)
-			tri_X_coordinates1[i] += (offset*velocity);
-		for(int i=0; i<tri_X_coordinates2.length; i++)
-			tri_X_coordinates2[i] += (offset*velocity);
-		for(int i=0; i<tri_X_coordinates3.length; i++)
-			tri_X_coordinates3[i] += (offset*velocity);
-	}
 	
-	 public void changeY(int offset, int velocity) {
-		 
-		 
-		 if(rect_y_coordinate_player>200) {
-			 rect_y_coordinate_player +=(offset*velocity);
-			 tri_y_coordinates_player[0] +=(offset*velocity);
-			 tri_y_coordinates_player[1] +=(offset*velocity);
-			 tri_y_coordinates_player[2] +=(offset*velocity);
-		 }
-		 
-		 else {
-			 rect_y_coordinate_player +=(-offset*10);
-			 tri_y_coordinates_player[0] +=(-offset*10);
-			 tri_y_coordinates_player[1] +=(-offset*10);
-			 tri_y_coordinates_player[2] +=(-offset*10);
-		 }
-		 
-		 /*
-		 for(int i=0; i<tri_y_coordinates_player.length; i++) {
-			 if(rect_y_coordinate_player>200) {
-				 
-				 tri_y_coordinates_player[i] +=(offset*velocity);
-			 }
-			 else {
-				 //System.out.println(tri_y_coordinates_player[i]);
-				 tri_y_coordinates_player[i] +=(-offset*velocity);
-			 }
-
-	    }
-	    */
-
 	
-	 }
-	
+	//super.paint(g);
+    //Graphics2D g2d = (Graphics2D) g;
+    //g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+    //        RenderingHints.VALUE_ANTIALIAS_ON);
+    //g2d.fillOval(x, y, 30, 30);
+    //g2d.fillRect(100,100,30,30);
+    //if(evilflagtaken==0)
+    //    g2d.drawRect(evilflagx, evilflagy, 30, 30);
+   // g2d.drawOval(400, 100, 30, 30);
 
 }
--- src/SingletonFlag.java
+++ src/SingletonFlag.java
@@ -1,14 +1,3 @@
-package designProject;
-
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import java.awt.event.*;
-
-
-
 import java.awt.Graphics2D;
 
 
